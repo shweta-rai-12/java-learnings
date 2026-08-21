3d7da74ecb0022c3743b7a77a029b81b792e624d
Shweta.Rai <shweta.rai@everbridge.com>
2026-08-22 00:25:31 +0530

    Strings

---
+++ src/dsa/strings/LargestOddNumInString.java
@@ -0,0 +1,8 @@
+package dsa.strings;
+
+public class LargestOddNumInString {
+
+    public static void main(String[] args){
+        
+    }
+}
--- src/dsa/strings/PalindromeCheck.java
+++ src/dsa/strings/PalindromeCheck.java
@@ -2,6 +2,7 @@ package dsa.strings;
 
 public class PalindromeCheck {
 
+    // Using two pointers
     public static void main(String[] args) {
         String str = "A man, a plan, a canal: Panama";
         System.out.println("Is the string a palindrome? : " + isPalindrome(str));
@@ -9,6 +10,21 @@ public class PalindromeCheck {
 
     // Reverse words in a given string / Palindrome check
     public static boolean isPalindrome(String s){
-        return false;
+        String str = s.toLowerCase();
+        int l=0, r=str.length()-1;
+        while(l<r){
+            if(!Character.isLetterOrDigit(str.charAt(l)))
+                l++;
+            else if(!Character.isLetterOrDigit(str.charAt(r)))
+                r--;
+            else if(str.charAt(l)!=str.charAt(r))
+                return false;
+            else {
+                r--;
+                l++;
+            }
+
+        }
+        return true;
     }
 }
